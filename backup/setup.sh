176eb18f76f6ea73fc5ea3fe21c7e1d363023881
realcryptonight <79969016+realcryptonight@users.noreply.github.com>
2021-03-15 14:43:19 +0100

    First update to make the backup system use parameters instead of user input. (#1)
    * Changes to transfer from user input to parameter input
    * Couple of changes to allow for other ssl certificate then LetsEncrypt
    * Couple of changes to the way invalid args are handled
    * Couple of changes that should solve the unresovled issues
    * First attempt to release
    
    Note: When no user is yet added it will give an error on the client side. (GnuTLS-fout -15: An unexpected TLS packet was received.
    )

--- backup/setup.sh
+++ backup/setup.sh
@@ -1,34 +1,66 @@
 #!/bin/bash
 
-# Ask user for deploy domain.
-echo "What is the deploy domain? example: deploy.danielmarkink.nl"
-read deploydomain
-echo $deploydomain > /deploy.txt
+# Check if all args are given
+if [ -z "$1" ]
+then
+	echo "Invalid arguments. Use: ./setup.sh <deployment server address> <is VMware VM yes|no> <backup domain> <use certbot for ssl yes|no> <backup disk (Optional)>"
+    exit 1
+else
+    echo $1 > /deploy.txt
+fi
+
+if [ -z "$3" ]
+then
+	echo "Invalid arguments. Use: ./setup.sh <deployment server address> <is VMware VM yes|no> <backup domain> <use certbot for ssl yes|no> <backup disk (Optional)>"
+    exit 1
+else
+	echo ''
+fi
+
+if [ -z "$5" ]
+then
+	adddisk=0
+else
+	adddisk=1
+fi
+
+if [[ "$adddisk" == 1 ]]
+then
+	(echo n; echo ""; echo ""; echo ""; echo ""; echo w; echo q) | fdisk $(echo $5)
+	mkfs.ext4 $5
+	mkdir /backups
+	mount $5 /backups
+	echo "${5} /backups ext4 defaults 1 2" >> /etc/fstab
+else
+	echo ''
+fi
 
 # Get the deploy domain.
 dpdomain=`cat /deploy.txt`
 
-echo "Is this a VMware VM?"
-read input
-case $input in
+case $4 in
 	[yY][eE][sS]|[yY])
-		# Since it is a VMware VM server we need to add a new backup disk.
-		diskinfo=`fdisk -l | grep Disk`
-		printf "${diskinfo}"
-		read -p "Where is the backup disk located? " disk
-		(echo n; echo ""; echo ""; echo ""; echo ""; echo w; echo y; echo q) | fdisk $(echo $disk)
-		mkfs.ext4 $disk
-		mkdir /backups
-		mount $disk /backups
-		echo "${disk} /backups ext4 defaults 1 2" >> /etc/fstab
+		echo ''
+	;;
+	[nN][oO]|[nN])
+		echo ''
+	;;
+	*)
+		echo "Invalid arguments. Use: ./setup.sh <deployment server address> <is VMware VM yes|no> <backup domain> <use certbot for ssl yes|no> <backup disk (Optional)>"
+		exit 1
+	;;
+esac
+
+case $2 in
+	[yY][eE][sS]|[yY])
 		# Install the default settings for the VMware VM server.
-		wget -O standard-settings.sh https://$dpdomain/debian/reuse-scripts/vmware/scripts/vmware-settings.sh
+		wget -O vmware-settings.sh https://$dpdomain/debian/reuse-scripts/vmware/scripts/vmware-settings.sh
 		chmod 777 vmware-settings.sh
 		./vmware-settings.sh
 		# Install and setup the vsFTPd server.
 		wget -O setup-vsftpd.sh https://$dpdomain/debian/reuse-scripts/standard/scripts/setup-vsftpd.sh
 		chmod 777 setup-vsftpd.sh
-		./setup-vsftpd.sh
+		./setup-vsftpd.sh $3 $4
 	;;
 	[nN][oO]|[nN])
 		# Install the default settings for the server.
@@ -40,10 +72,10 @@ case $input in
 		# Install and setup the vsFTPd server.
 		wget -O setup-vsftpd.sh https://$dpdomain/debian/reuse-scripts/standard/scripts/setup-vsftpd.sh
 		chmod 777 setup-vsftpd.sh
-		./setup-vsftpd.sh
+		./setup-vsftpd.sh $3 $4
 	;;
 	*)
-		echo "Invalid input..."
+		echo "Invalid arguments. Use: ./setup.sh <deployment server address> <is VMware VM yes|no> <backup domain> <use certbot for ssl yes|no> <backup disk (Optional)>"
 		exit 1
 	;;
 esac
@@ -52,6 +84,24 @@ esac
 wget -O 10-sysinfo https://$dpdomain/debian/backup/10-sysinfo
 mv 10-sysinfo /etc/update-motd.d/
 chmod 777 /etc/update-motd.d/*
+
+case $4 in
+	[yY][eE][sS]|[yY])
+		echo "All done"
+	;;
+	[nN][oO]|[nN])
+		echo "We are almost done. You only need to add the SSL Cetificate and start vsFTPd"
+		echo "Go to /etc/certs/$3/ and add the following files:"
+		echo "The full chain as 'fullchain.pem' and th private key as 'privkey.pem'"
+		echo "After that type: 'systemctl start vsftpd'"
+	;;
+	*)
+		echo "We are almost done. You only need to add the SSL Cetificate and start vsFTPd"
+		echo "Go to /etc/certs/$3/ and add the following files:"
+		echo "The full chain as 'fullchain.pem' and th private key as 'privkey.pem'"
+		echo "After that type: 'systemctl start vsftpd'"
+	;;
+esac
 
 # Clean up.
 rm setup.sh
