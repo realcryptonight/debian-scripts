7e545cc8e27d7676666f72df474ad72fbe7f2497
realcryptonight <me@realcryptonight.dev>
2021-07-16 16:44:47 +0200

    Updated to contain all new changes on my production version

--- backup/setup.sh
+++ backup/setup.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-# Ask user for deploy domain.
-echo "What is the deploy domain? example: deploy.danielmarkink.nl"
-read deploydomain
-echo $deploydomain > /deploy.txt
+# Check if all args are given
+if [ -z "$1" ]
+then
+	echo "Invalid arguments. Use: ./setup.sh <deployment server address> <is VMware VM yes|no> <backup disk (Optional)>"
+    exit 1
+else
+    echo $1 > /deploy.txt
+fi
+
+if [ -z "$3" ]
+then
+	adddisk=0
+else
+	adddisk=1
+fi
+
+if [[ "$adddisk" == 1 ]]
+then
+	(echo n; echo ""; echo ""; echo ""; echo ""; echo w; echo q) | fdisk $(echo $3)
+	mkfs.ext4 $3
+	mkdir /backups
+	mount $3 /backups
+	echo "${3} /backups ext4 defaults 1 2" >> /etc/fstab
+else
+	echo ''
+fi
 
 # Get the deploy domain.
 dpdomain=`cat /deploy.txt`
 
-echo "Is this a VMware VM?"
-read input
-case $input in
+case $2 in
 	[yY][eE][sS]|[yY])
-		# Since it is a VMware VM server we need to add a new backup disk.
-		diskinfo=`fdisk -l | grep Disk`
-		printf "${diskinfo}"
-		read -p "Where is the backup disk located? " disk
-		(echo n; echo ""; echo ""; echo ""; echo ""; echo w; echo y; echo q) | fdisk $(echo $disk)
-		mkfs.ext4 $disk
-		mkdir /backups
-		mount $disk /backups
-		echo "${disk} /backups ext4 defaults 1 2" >> /etc/fstab
 		# Install the default settings for the VMware VM server.
-		wget -O standard-settings.sh https://$dpdomain/debian/reuse-scripts/vmware/scripts/vmware-settings.sh
+		wget -O vmware-settings.sh https://$dpdomain/debian/reuse-scripts/vmware/scripts/vmware-settings.sh
 		chmod 777 vmware-settings.sh
 		./vmware-settings.sh
-		# Install and setup the vsFTPd server.
-		wget -O setup-vsftpd.sh https://$dpdomain/debian/reuse-scripts/standard/scripts/setup-vsftpd.sh
-		chmod 777 setup-vsftpd.sh
-		./setup-vsftpd.sh
 	;;
 	[nN][oO]|[nN])
 		# Install the default settings for the server.
@@ -37,16 +44,26 @@ case $input in
 		./standard-settings.sh
 		# Create the location that on the VMware VM an new disk would be.
 		mkdir /backups
-		# Install and setup the vsFTPd server.
-		wget -O setup-vsftpd.sh https://$dpdomain/debian/reuse-scripts/standard/scripts/setup-vsftpd.sh
-		chmod 777 setup-vsftpd.sh
-		./setup-vsftpd.sh
 	;;
 	*)
-		echo "Invalid input..."
+		echo "Invalid arguments. Use: ./setup.sh <deployment server address> <is VMware VM yes|no> <backup disk (Optional)>"
 		exit 1
 	;;
 esac
+
+wget https://$dpdomain/debian/reuse-scripts/standard/scripts/add_ssh_user.sh
+chmod 700 add_ssh_user.sh
+chown root:root /backups
+
+groupadd sftpgroup
+sed -i 's/Subsystem/#Subsystem/g' /etc/ssh/sshd_config
+echo "Subsystem sftp internal-sftp" >> /etc/ssh/sshd_config
+echo "   Match Group sftpgroup" >> /etc/ssh/sshd_config
+echo "   ChrootDirectory /backups" >> /etc/ssh/sshd_config
+echo "   ForceCommand internal-sftp" >> /etc/ssh/sshd_config
+echo "   X11Forwarding no" >> /etc/ssh/sshd_config
+echo "   AllowTcpForwarding no" >> /etc/ssh/sshd_config
+service sshd restart
 
 # Add the 10-sysinfo for the backup server.
 wget -O 10-sysinfo https://$dpdomain/debian/backup/10-sysinfo
